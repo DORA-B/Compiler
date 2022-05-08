6bf75b648be0022600404e464206072b1480af76
DORA-B <2358543632@qq.com>
2022-05-08 09:42:16 +0800

    添加项目文件。

---
+++ LR_analytics/LR1_main.cpp
@@ -0,0 +1,37 @@
+#include "LR1.h"
+
+
+int main() {
+	LR1 lr1;
+	init_map();
+	/*词法分析*/
+	lr1.lex.lex_analyze("input.txt");
+	/*语法分析*/
+	lr1.lex.output_result("Lexoutput.txt");
+
+	string dat_path = "grammer.txt";
+	lr1.read_generators(dat_path);
+	lr1.get_firstset_of_vn();
+	//for (int i = 0; i < lr1.generator.size(); ++i)
+	//	lr1.get_firstset_of_string(lr1.generator[i].Right);
+
+	lr1.init_items();
+	//int i = 0;
+	//for (auto a = lr1.eventclo.begin(); a != lr1.eventclo.end(); a++)
+	//{
+	//	cout << "closure" << i++ << endl;
+	//	for (auto b = a->Es.begin(); b != a->Es.end(); b++)
+	//	{
+	//		cout << *b << endl;
+	//	}
+	//	cout << endl;
+	//}
+
+	lr1.getTable();
+
+	lr1.printTable("resultTable.txt");
+	lr1.grammartree("tree.dot",lr1.lex.Result);
+	lr1.SemanticAnalysis.printQuadruple("quadrupleresult.txt");
+
+	return 0;
+}
